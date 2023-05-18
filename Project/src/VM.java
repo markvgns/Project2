984d72b2042a1f8fabd1af18b9ccb3a7c5bbfffb
Sloudis <stavrosloudis@gmail.com>
2023-05-18 14:49:33 +0300

    ok

--- Project/src/VM.java
+++ Project/src/VM.java
@@ -1,8 +1,8 @@
 public class VM extends ComputerCluster {
-  int VmId;
-  int VMCPU;
-  int VMRAM;
-  String VMSoftware;
+  private int VmId;
+  private int VMCPU;
+  private int VMRAM;
+  private String VMSoftware;
 
   public VM(int VmId, int VMCPU, int VMRAM, String VMSoftware) {
     this.VmId = VmId;
@@ -10,6 +10,38 @@ public class VM extends ComputerCluster {
     this.VMRAM = VMRAM;
     this.VMSoftware = VMSoftware;
 
+  }
+
+  public int getVmId() {
+    return VmId;
+  }
+
+  public void setVmId(int vmId) {
+    VmId = vmId;
+  }
+
+  public int getVMCPU() {
+    return VMCPU;
+  }
+
+  public void setVMCPU(int vMCPU) {
+    VMCPU = vMCPU;
+  }
+
+  public int getVMRAM() {
+    return VMRAM;
+  }
+
+  public void setVMRAM(int vMRAM) {
+    VMRAM = vMRAM;
+  }
+
+  public String getVMSoftware() {
+    return VMSoftware;
+  }
+
+  public void setVMSoftware(String vMSoftware) {
+    VMSoftware = vMSoftware;
   }
 
 }
