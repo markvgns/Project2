07a261e1ca043495280df7a0443ae736237a177f
Sloudis <stavrosloudis@gmail.com>
2023-05-18 16:56:36 +0300

    ok

--- Project/src/CLI.java
+++ Project/src/CLI.java
@@ -4,6 +4,11 @@ import java.util.Random;
 public class CLI {
 
   ArrayList<VM> VMs = new ArrayList<>();
+  public int REMAIN_CPU = ComputerCluster.Max_CPU;
+  public int REMAIN_RAM = ComputerCluster.Max_RAM;
+  public int REMAIN_SSD = ComputerCluster.Max_SSD;
+  public int REMAIN_GPU = ComputerCluster.Max_GPU;
+  public int REMAIN_Ethernet = ComputerCluster.Max_Ethernet;
 
   // VM
   public void createVm(int VMCPU, int VMRAM, String VMSoftware) {
@@ -15,6 +20,7 @@ public class CLI {
     }
     VM tempVm = new VM(VmID, VMCPU, VMRAM, VMSoftware);
     VMs.add(tempVm);
+    updateremaining(VMCPU, VMRAM, 0, 0, 0);
 
   }
 
@@ -28,6 +34,7 @@ public class CLI {
     }
     PlainVM tempVm = new PlainVM(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD);
     VMs.add(tempVm);
+    updateremaining(VMCPU, VMRAM, PlainSSD, 0, 0);
 
   }
 
@@ -41,6 +48,7 @@ public class CLI {
     }
     VmGPU tempVm = new VmGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, VmGPU);
     VMs.add(tempVm);
+    updateremaining(VMCPU, VMRAM, PlainSSD, 0, VmGPU);
 
   }
 
@@ -54,6 +62,7 @@ public class CLI {
     }
     VMNetworked tempVm = new VMNetworked(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth);
     VMs.add(tempVm);
+    updateremaining(VMCPU, VMRAM, PlainSSD, Bandwidth, 0);
 
   }
 
@@ -67,15 +76,23 @@ public class CLI {
     }
     VMNetworkedGPU tempVm = new VMNetworkedGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth, VMGPU);
     VMs.add(tempVm);
+    updateremaining(VMCPU, VMRAM, PlainSSD, Bandwidth, VMGPU);
 
   }
 
   public int createRandomInt() {
 
     Random random = new Random();
-    int randomInt = random.nextInt(Integer.MAX_VALUE - 1) + 1;
+    int randomInt = random.nextInt(Integer.MAX_VALUE - 2147483000) + 1;
     return randomInt;
 
   }
 
+  public void updateremaining(int VMCPU, int VMRAM, int PlainSSD, int Bandwidth, int gpu) {
+    REMAIN_CPU -= VMCPU;
+    REMAIN_RAM -= VMRAM;
+    REMAIN_SSD -= PlainSSD;
+    REMAIN_Ethernet -= Bandwidth;
+    REMAIN_GPU -= gpu;
+  }
 }
--- Project/src/ComputerCluster.java
+++ Project/src/ComputerCluster.java
@@ -1,18 +1,8 @@
 public class ComputerCluster {
-    int CPU = 128;
-    int RAM = 256;
-    int SSD = 2048;
-    int GPU = 8;
-    int Ethernet = 320;
-
-    int REMAIN_CPU ;
-    int REMAIN_RAM ;
-    int REMAIN_SSD ;
-    int REMAIN_GPU ;
-    int REMAIN_Ethernet ;
-
-    
-
-  
+    public static final int Max_CPU = 128;
+    public static final int Max_RAM = 256;
+    public static final int Max_SSD = 2048;
+    public static final int Max_GPU = 8;
+    public static final int Max_Ethernet = 320;
 
 }
