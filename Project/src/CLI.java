bfb2dca3d2ecd7fb639a925c09ffd46a914fd3cd
markvgns <133893812+markvgns@users.noreply.github.com>
2023-05-18 14:21:47 +0300

    Update CLI.java

--- Project/src/CLI.java
+++ Project/src/CLI.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CLI {
 
   ArrayList<VM> VMs = new ArrayList<>();
   int numofVms = 0;
+  public int VmID;
 
   // VM
   public void createVm(int VMCPU, int VMRAM, String VMSoftware) {
-    int VmID = 0;
+    int VmID = createRandomInt();
     VM tempVm = new VM(VmID, VMCPU, VMRAM, VMSoftware);
     VMs.add(tempVm);
     numofVms++;
@@ -15,7 +17,7 @@ public class CLI {
 
   // PlainVM
   public void createPlainVm(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD) {
-    int VmID = 0;
+    int VmID = createRandomInt();
     PlainVM tempVm = new PlainVM(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD);
     VMs.add(tempVm);
     numofVms++;
@@ -23,7 +25,7 @@ public class CLI {
 
   // VmGPU
   public void createVmGPU(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int VmGPU) {
-    int VmID = 0;
+    int VmID = createRandomInt();
     VmGPU tempVm = new VmGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, VmGPU);
     VMs.add(tempVm);
     numofVms++;
@@ -31,7 +33,7 @@ public class CLI {
 
   // VmNetworked
   public void createVmNetworked(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int Bandwidth) {
-    int VmID = 0;
+    int VmID = createRandomInt();
     VMNetworked tempVm = new VMNetworked(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth);
     VMs.add(tempVm);
     numofVms++;
@@ -40,10 +42,29 @@ public class CLI {
 
   // VmNetworkedGPU
   public void createVmNetworkedGPU(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int Bandwidth, int VMGPU) {
-    int VmID = 0;
+    int VmID = createRandomInt();
     VMNetworkedGPU tempVm = new VMNetworkedGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth, VMGPU);
     VMs.add(tempVm);
     numofVms++;
+  }
+
+  public int createRandomInt()
+  {
+    for(int i=0; i<numofVms; i++)
+    {
+        if(VmID == VMs.get(i).getID()) /*-------> sou bgazei error epeidh den exoume ftiaxei getter gia to id se kathe class ton VMs*/
+        {
+            break;
+        }
+        else 
+        {
+            Random random = new Random();
+            int randomInt = random.nextInt(Integer.MAX_VALUE -1) +1;
+            return randomInt;
+
+        }
+    }
+    
   }
 
 }
