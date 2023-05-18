861d8b9d009e1a508922a8890c8a23ee36a49a4f
Sloudis <stavrosloudis@gmail.com>
2023-05-18 14:49:46 +0300

    ok

--- Project/src/CLI.java
+++ Project/src/CLI.java
@@ -1,49 +1,81 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CLI {
 
   ArrayList<VM> VMs = new ArrayList<>();
-  int numofVms = 0;
 
   // VM
   public void createVm(int VMCPU, int VMRAM, String VMSoftware) {
-    int VmID = 0;
+    int VmID = createRandomInt();
+    for (int i = 0; i < VMs.size(); i++) {
+      if (VmID == VMs.get(i).getVmId()) {
+        VmID = createRandomInt();
+      }
+    }
     VM tempVm = new VM(VmID, VMCPU, VMRAM, VMSoftware);
     VMs.add(tempVm);
-    numofVms++;
+
   }
 
   // PlainVM
   public void createPlainVm(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD) {
-    int VmID = 0;
+    int VmID = createRandomInt();
+    for (int i = 0; i < VMs.size(); i++) {
+      if (VmID == VMs.get(i).getVmId()) {
+        VmID = createRandomInt();
+      }
+    }
     PlainVM tempVm = new PlainVM(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD);
     VMs.add(tempVm);
-    numofVms++;
+
   }
 
   // VmGPU
   public void createVmGPU(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int VmGPU) {
-    int VmID = 0;
+    int VmID = createRandomInt();
+    for (int i = 0; i < VMs.size(); i++) {
+      if (VmID == VMs.get(i).getVmId()) {
+        VmID = createRandomInt();
+      }
+    }
     VmGPU tempVm = new VmGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, VmGPU);
     VMs.add(tempVm);
-    numofVms++;
+
   }
 
   // VmNetworked
   public void createVmNetworked(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int Bandwidth) {
-    int VmID = 0;
+    int VmID = createRandomInt();
+    for (int i = 0; i < VMs.size(); i++) {
+      if (VmID == VMs.get(i).getVmId()) {
+        VmID = createRandomInt();
+      }
+    }
     VMNetworked tempVm = new VMNetworked(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth);
     VMs.add(tempVm);
-    numofVms++;
 
   }
 
   // VmNetworkedGPU
   public void createVmNetworkedGPU(int VMCPU, int VMRAM, String VMSoftware, int PlainSSD, int Bandwidth, int VMGPU) {
-    int VmID = 0;
+    int VmID = createRandomInt();
+    for (int i = 0; i < VMs.size(); i++) {
+      if (VmID == VMs.get(i).getVmId()) {
+        VmID = createRandomInt();
+      }
+    }
     VMNetworkedGPU tempVm = new VMNetworkedGPU(VmID, VMCPU, VMRAM, VMSoftware, PlainSSD, Bandwidth, VMGPU);
     VMs.add(tempVm);
-    numofVms++;
+
+  }
+
+  public int createRandomInt() {
+
+    Random random = new Random();
+    int randomInt = random.nextInt(Integer.MAX_VALUE - 1) + 1;
+    return randomInt;
+
   }
 
 }
